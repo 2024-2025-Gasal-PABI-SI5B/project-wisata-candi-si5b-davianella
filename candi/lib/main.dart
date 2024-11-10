3f3890681d9656ee475e6123902f2e7d1c9724fb
davianella <daviandaaa@gmail.com>
2024-11-10 22:38:12 +0700

    otw detail screen

---
+++ candi/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:candi/screens/detail_screen.dart';
+import 'package:candi/data/candi_data.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Wisata Candi',
+      theme: ThemeData.(
+        colorScheme : ColorScheme.fromSeed(seedColor:Color.deepPurple),
+      useMaterial3: true
+      ),
+      home: DetailScreen(
+        candi: candiList[0],
+        ),
+    );
+  }
+}
+
+
---
+++ candi/lib/screens/detail_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:candi/models/candi.dart';
+
+class DetailScreen extends StatelessWidget {
+  final Candi candi;
+
+  const DetailScreen({super.key, required this.candi});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+      
+      ),
+    );
+  }
+  
+}
