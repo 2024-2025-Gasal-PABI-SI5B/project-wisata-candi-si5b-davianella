bcb1267db63c20540501ff2424e0557d6a523a01
davianella <daviandaaa@gmail.com>
2024-12-02 11:35:58 +0700

    navbar+home

--- candi/lib/main.dart
+++ candi/lib/main.dart
@@ -1,9 +1,5 @@
-import 'package:candi/screens/profile_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:candi/screens/detail_screen.dart';
-import 'package:candi/data/candi_data.dart';
-import 'package:candi/screens/signin_screen.dart';
-import 'package:candi/screens/signup_screen.dart';
+import 'package:candi/screens/main_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -14,24 +10,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Wisata Candi',
-      theme: ThemeData(
-        appBarTheme: const AppBarTheme(
-          iconTheme: IconThemeData(color: Colors.deepPurple),
-          titleTextStyle: TextStyle(
-            color: Colors.deepPurple,
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple).copyWith(
-          primary: Colors.deepPurple,
-          surface: Colors.deepPurple[50],
-        ),
-        useMaterial3: true,
-      ),
-      home: SignUpScreen(),
-    );
+    return const MaterialApp(home: MainScreen());
   }
 }
